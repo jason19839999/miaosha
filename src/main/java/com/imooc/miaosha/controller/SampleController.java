10537a7373fb6f9bd76309df7d8e9e496be2a4ce
jason <shicong.zhang@juniorchina.com>
2018-08-20 17:26:46 +0800

    添加@Service注解，如果是@AutoWire ,需要加上@Service

--- src/main/java/com/imooc/miaosha/controller/SampleController.java
+++ src/main/java/com/imooc/miaosha/controller/SampleController.java
@@ -1,15 +1,5 @@
 package com.imooc.miaosha.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.imooc.miaosha.domain.User;
 import com.imooc.miaosha.rabbitmq.MQSender;
 import com.imooc.miaosha.redis.RedisService;
@@ -17,21 +7,30 @@ import com.imooc.miaosha.redis.UserKey;
 import com.imooc.miaosha.result.CodeMsg;
 import com.imooc.miaosha.result.Result;
 import com.imooc.miaosha.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/demo")
 public class SampleController {
 
-	@Autowired
-	UserService userService;
-	
-	@Autowired
-	RedisService redisService;
+    @Autowired
+    UserService userService;
 
-	@Autowired
-	MQSender sender;
-	
-//	@RequestMapping("/mq/header")
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    MQSender sender;
+
+    //	@RequestMapping("/mq/header")
 //    @ResponseBody
 //    public Result<String> header() {
 //		sender.sendHeader("hello,imooc");
@@ -52,79 +51,79 @@ public class SampleController {
 //        return Result.success("Hello，world");
 //    }
 //	
-	@RequestMapping("/mq")
+    @RequestMapping("/mq")
     @ResponseBody
     public Result<String> mq() {
-		sender.send("hello,imooc");
+        sender.send("hello,imooc");
         return Result.success("Hello，world");
     }
-	
+
     @RequestMapping("/hello")
     @ResponseBody
     public Result<String> home() {
         return Result.success("Hello，world");
     }
-    
+
     @RequestMapping("/error")
     @ResponseBody
     public Result<String> error() {
         return Result.error(CodeMsg.SESSION_ERROR);
     }
-    
+
     @RequestMapping("/hello/themaleaf")
     public String themaleaf(Model model) {
         model.addAttribute("name", "Joshua");
         return "hello";
     }
-    
+
     @RequestMapping("/db/get")
     @ResponseBody
     public Result<User> dbGet() {
-    	User user = userService.getById(1);
+        User user = userService.getById(1);
         return Result.success(user);
     }
-    
-    
+
+
     @RequestMapping("/db/tx")
     @ResponseBody
     public Result<Boolean> dbTx() {
-    	userService.tx();
+        userService.tx();
         return Result.success(true);
     }
-    
+
     @RequestMapping("/redis/get")
     @ResponseBody
     public Result<User> redisGet() {
-    	User  user  = redisService.get(UserKey.getById, ""+1, User.class);
+        User user = redisService.get(UserKey.getById, "" + 1, User.class);
         return Result.success(user);
     }
-    
+
     @RequestMapping("/redis/set")
     @ResponseBody
     public Result<Boolean> redisSet() {
-    	User user  = new User();
-    	user.setId(1);
-    	user.setName("1111");
-    	redisService.set(UserKey.getById, ""+1, user);//UserKey:id1
+        User user = new User();
+        user.setId(1);
+        user.setName("1111");
+        redisService.set(UserKey.getById, "" + 1, user);//UserKey:id1
         return Result.success(true);
     }
-    
+
     @RequestMapping("/redis/setnxex")
     @ResponseBody
     public Result<String> redisSetnxex() {
-    	User user  = new User();
-    	user.setId(1);
-    	user.setName("1111");
-    	boolean ret1 = redisService.setNXEX(UserKey.getById, ""+1, user);
-    	boolean ret2 = redisService.setNXEX(UserKey.getById, ""+1, user);
-        return Result.success(ret1+","+ret2);
+        User user = new User();
+        user.setId(1);
+        user.setName("1111");
+        boolean ret1 = redisService.setNXEX(UserKey.getById, "" + 1, user);
+        boolean ret2 = redisService.setNXEX(UserKey.getById, "" + 1, user);
+        return Result.success(ret1 + "," + ret2);
     }
-    
+
     @RequestMapping("/array")
     @ResponseBody
-    public Result<List<String>> array(@RequestParam("names")String[] names) {
+    public Result<List<String>> array(@RequestParam("names") String[] names) {
         return Result.success(Arrays.asList(names));
     }
-    
-    
+
+
 }
