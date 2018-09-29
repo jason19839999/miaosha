f45d7e9287c0d0450ec6bb8c3bdb8a3a9ab47405
jason <shicong.zhang@juniorchina.com>
2018-09-29 16:11:38 +0800

    秒杀---------预减库存添加分布式锁

--- src/main/java/com/imooc/miaosha/rabbitmq/MQConfig.java
+++ src/main/java/com/imooc/miaosha/rabbitmq/MQConfig.java
@@ -12,12 +12,16 @@ public class MQConfig {
 	
 	public static final String MIAOSHA_QUEUE = "miaosha.queue";
 	public static final String QUEUE = "queue";
+
 	public static final String TOPIC_QUEUE1 = "topic.queue1";
 	public static final String TOPIC_QUEUE2 = "topic.queue2";
-	public static final String HEADER_QUEUE = "header.queue";
 	public static final String TOPIC_EXCHANGE = "topicExchage";
-	public static final String FANOUT_EXCHANGE = "fanoutxchage";
+
+	public static final String HEADER_QUEUE = "header.queue";
 	public static final String HEADERS_EXCHANGE = "headersExchage";
+
+	public static final String FANOUT_EXCHANGE = "fanoutxchage";
+
 	
 //	@Bean
 //	public ConnectionFactory connectionFactory() {
