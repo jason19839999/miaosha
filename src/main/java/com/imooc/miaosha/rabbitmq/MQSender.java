5ab9ed52b4154590355605111dea1e2d12241ee3
jason <shicong.zhang@juniorchina.com>
2018-11-08 14:54:08 +0800

    amqp增加发送成功与否回调函数

--- src/main/java/com/imooc/miaosha/rabbitmq/MQSender.java
+++ src/main/java/com/imooc/miaosha/rabbitmq/MQSender.java
@@ -3,6 +3,8 @@ package com.imooc.miaosha.rabbitmq;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,18 @@ public class MQSender {
 	
 	@Autowired
 	AmqpTemplate amqpTemplate ;
+
+	//发送成功之后回调函数
+	final RabbitTemplate.ConfirmCallback confirmCallback  = new RabbitTemplate.ConfirmCallback() {
+		@Override
+		public void confirm(CorrelationData correlationData, boolean ack, String s) {
+            if(ack){
+            	//如果confirm返回成功，则进行更新表数据，更新状态为发送成功1；
+			}else{
+            	//如果失败，则进行重试或者补偿等手段；目前用一个定时任务定时轮训发送状态为0的，重试3次，如果还不成功就改为2，发送失败，人工干预了。
+			}
+		}
+	};
 	
 	public void sendMiaoshaMessage(MiaoshaMessage mm) {
 		String msg = RedisService.beanToString(mm);
